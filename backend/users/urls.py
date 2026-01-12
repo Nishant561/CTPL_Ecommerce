d67f905605986283ee3149a265cab7651dbebbd9
Sabita Tiwari <sabita.tiwari613@gmail.com>
2026-01-12 16:49:37 +0545

    Add Django REST backend

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
-urlpatterns=[
-    
+router = DefaultRouter()
+router.register(r'', UserViewSet)  # '' because we include this under api/users/
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
