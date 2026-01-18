e136ebf93c25751f29b0d78b1964a21530d76a90
Sabita Tiwari <sabita.tiwari613@gmail.com>
2026-01-18 12:46:18 +0545

    Make products and categories public

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CategoryListView, CategoryCreateView, ProductListView, ProductCreateView
+from .views import CategoryListView, CategoryCreateView, ProductListView, ProductCreateView, ProductDetailView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('categories/create/', CategoryCreateView.as_view(), name='category-create'),
     path('', ProductListView.as_view(), name='product-list'),
     path('create/', ProductCreateView.as_view(), name='product-create'),
+    path('<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
